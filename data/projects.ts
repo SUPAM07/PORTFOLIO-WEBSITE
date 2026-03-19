13bc10d12593ad09a7cc36ee114ea9d854452a60
Supam Roy <supamroy8@gmail.com>
2026-03-20 03:18:41 +0530

    added projects

--- data/projects.ts
+++ data/projects.ts
@@ -3,42 +3,44 @@ import { Project } from '@/types';
 // Exporting projects vector
 export const projects: Project[] = [
   {
-    title: 'Driwwwle',
+    title: 'ThrottleX',
     description:
-      'Discover creative websites and developers. A portal for you to share your projects.',
-    image: '/logos/driwwwle.svg',
-    tags: ['react', 'nextjs', 'typescript', 'mongodb', 'tailwindcss'],
-    sourceCode: 'https://github.com/itsnitinr/driwwwle',
+      'High-performance distributed rate limiter (Node.js + Redis) with 796k+ req/sec and adaptive traffic control.',
+    image: '/logos/ThrottleX.png',
+    tags: ['Node.js', 'Redis', 'typescript', 'lua', 'prometheus', 'grafana'],
+    sourceCode: 'https://github.com/SUPAM07/ThrottleX',
     demo: 'https://driwwwle.com',
-    slug: 'driwwwle',
+    slug: 'throttlex',
   },
   {
     title: 'VSCode Portfolio',
     description:
       'A Visual Studio Code themed developer portfolio built with Next.js and CSS Modules.',
-    image: '/logos/vsc.svg',
+    image: '/logos/home.png',
     tags: ['react', 'nextjs', 'css-modules', 'typescript'],
-    sourceCode: 'https://github.com/itsnitinr/vscode-portfolio',
-    demo: 'https://vscode-portfolio.vercel.app',
+    sourceCode: 'https://github.com/SUPAM07/PORTFOLIO-WEBSITE',
+    demo: 'https://portfolio-website-vueh.vercel.app/',
     slug: 'vscode-portfolio',
   },
   {
-    title: 'Subtrackt',
+    title: 'GenUI',
     description:
-      'A simple and elegant way to track your subscriptions and save money.',
-    image: '/logos/subtrackt.svg',
-    tags: ['react', 'nodejs', 'express', 'postgresql', 'stripe'],
-    sourceCode: 'https://github.com/itsnitinr/subtrackt',
-    slug: 'subtrackt',
+      'Developed an AI-driven React component generator using Gemini API with Monaco-based live editor for real-time rendering and faster iteration.',
+    image: '/logos/GenUI.png',
+    tags: ['react', 'Tailwind CSS', 'Javascript', 'Gemini API'],
+    sourceCode: 'https://github.com/SUPAM07/Gen-UI',
+    demo: 'https://ai-component-generator-seven.vercel.app/',
+    slug: 'GENUI',
   },
   {
-    title: 'Coolify Deployments',
+    title: 'Rubiks Cube Solver',
     description:
-      'VSCode extension to track and deploy your Coolify applications.',
-    image: '/logos/coolify.svg',
-    tags: ['typescript', 'vscode-api', 'javascript'],
-    sourceCode: 'https://github.com/itsnitinr/coolify-vscode-extension',
-    slug: 'coolify-vscode-extension',
+      'Computer vision Rubik’s Cube solver with real-time detection and optimal solving.',
+    image: '/logos/project2.png',
+    tags: ['python', 'OpenCV', 'NumPy', 'kociemba'],
+    sourceCode: 'https://github.com/SUPAM07/Rubik-s-Cube-Solver-',
+    demo: 'https://drive.google.com/file/d/1e1Jehq7A7n3eJeUhSfEsecYDBnvVKpFK/view',
+    slug: 'cube-solver',
   },
 ];
 
